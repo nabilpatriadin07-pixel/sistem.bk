0d03c9526814c3044c23a6bdaec6789011a4c5d9
RizqiHid <dianriz472@gmail.com>
2026-09-03 22:19:00 +0700

    Pelanggaran siswa

--- BK/src/main/java/Model/pelanggaran_siswa.java
+++ BK/src/main/java/Model/pelanggaran_siswa.java
@@ -2,5 +2,16 @@
 package Model;
 
 public class pelanggaran_siswa {
-    
+    public date tanggal;
+    public int poin;
+    public String keterangan;
+    public String status;
+
+    public pelanggaran_siswa(date tanggal,int poin,String keterangan,String status) {
+        this.tanggal = tanggal;
+        this.poin = poin;
+        this.keterangan = keterangan;
+        this.status = status;
+}
+
 }
